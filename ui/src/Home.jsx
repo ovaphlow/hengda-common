65f7c0bbc21d53e70d0122dbb3a3a94887daf2af
ovaphlow <ovaphlow@live.com>
2020-09-26 16:18:27 +0800

    .

--- ui/src/Home.jsx
+++ ui/src/Home.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function Home() {
-  return <h1>HOME</h1>;
+  return (
+    <>
+      <header></header>
+
+      <main>
+        <h1>HOME</h1>
+      </main>
+
+      <footer></footer>
+    </>
+  );
 }
